bb481fe4ed5c7ccebeda1a9c2932db2bbb82c559
Martin Brychta <martin@brychta.name>
2012-12-27 20:47:17 +0100

    all clear, no comments (will do!)

---
+++ src/test/java/name/brychta/minecartremover/MinecartRemoverTest.java
@@ -0,0 +1,38 @@
+package name.brychta.minecartremover;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MinecartRemoverTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MinecartRemoverTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MinecartRemoverTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
